9cf2a0dab7ed95b4f0e41a79d1d66ef8a65808ed
binung <SG.binung715@gmail.com>
2024-05-28 19:51:13 +0200

    Vite IP Adress

--- app/Policies/ChirpPolicy.php
+++ app/Policies/ChirpPolicy.php
@@ -8,29 +8,6 @@ use App\Models\User;
 
 class ChirpPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Chirp $chirp): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
 
     /**
      * Determine whether the user can update the model.
@@ -46,21 +23,5 @@ class ChirpPolicy
     public function delete(User $user, Chirp $chirp): bool
     {
         return $this->update($user, $chirp);
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Chirp $chirp): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Chirp $chirp): bool
-    {
-        //
     }
 }
